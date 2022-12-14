62872278f9bf0d061dd27733a9085bc66acf08a3
Htxgamegh <116314659+Htxgamegh@users.noreply.github.com>
2022-12-14 11:41:52 -0300

    Add files via upload

---
+++ assets/preload/custom_events/Goodbye Hud.lua
@@ -0,0 +1,7 @@
+function onEvent(name,value1,value2)
+	if name == 'Goodbye Hud' then
+		doTweenAlpha('hudalpha', 'camHUD', tonumber(value1), tonumber(value2), 'linear');
+		doTweenAlpha('otheralpha', 'camOther', tonumber(value1), tonumber(value2), 'linear');
+		--debugPrint('Event triggered: ', name, duration, targetAlpha);
+	end
+end
